012e1e59cba4836b2ee611e07a5fe6f5d42fd984
Zahnen Garner <zahnen@gmail.com>
2020-11-05 10:46:47 -0800

    add final fixes to functions/result displays
    Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,16 @@ let player1 = new Player();
 let player2 = new Player ();
 
 function Player() {
-  this.turnCount = 0;
+  this.turnCount = 1;
   this.turnScore = 0;
-  this.totalScore = 0;
+  this.totalScore = 90;
   this.currentRoll;
 }
 
-Player.prototype.endTurn = function(){
+Player.prototype.endTurn = function() {
   this.totalScore += this.turnScore;
-  this.clearScore();
-  this.turnCount++
-}
-
-Player.prototype.clearScore = function() {
+  this.turnCount++;
   this.turnScore = 0;
-}
-
-Player.prototype.addScore = function(value) {
-  this.turnScore += value;
 }
 
 function rollDie(player) {
@@ -27,13 +19,45 @@ function rollDie(player) {
   player.currentRoll = result;
 
   if (result > 1) {
-    player.addScore(result);
+    player.turnScore += result;
     return true;
   }
   else {
-    player.clearScore();
+    player.turnScore = 0;
     player.endTurn();
     return false;
+  }
+}
+
+function endTurnPlayerOne() {
+  if (player1.totalScore >= 100) {
+    alert("Player 1 won the game!")
+    $("#gameField").hide();
+    $("#gameResult").show();
+    $("#winnerName").html($("input#playerOneName").val());
+  } else {
+    $("#rollResult-1").html(" ");
+    $("#turnScore-1").html(" ");
+    $("#total-1").html(player1.totalScore);
+    $("#player1").hide();
+    $("#player2").show();
+    $("#turnOne").html(player1.turnCount);
+  }
+}
+
+function endTurnPlayerTwo() {
+  if (player2.totalScore >= 100) {
+    alert("Player 2 won the game!")
+    $("#gameField").hide();
+    $("#gameResult").show();
+    $("#winnerName").html($("input#playerTwoName").val());
+  } else {
+    $("#rollResult-2").html(" ");
+    $("#turnScore-2").html(" ");
+    $("#total-2").html(player2.totalScore);
+    $("#player2").hide();
+    $("#player1").show();
+    $("#turnTwo").html(player2.turnCount);
   }
 }
 
@@ -42,61 +66,52 @@ $(document).ready(function() {
     event.preventDefault();
     $(".nameOne").text($("input#playerOneName").val());
     $(".nameTwo").text($("input#playerTwoName").val());
+    $("#turnOne").html(player1.turnCount);
+    $("#turnTwo").html(player2.turnCount);
     $("#player2").hide();
     $("#player1").show();
   });
+
   $("form#player1Play").submit(function(event) {
     event.preventDefault();
     let roll = rollDie(player1);
+
     if (roll) {
       $("#rollResult-1").html(" " + player1.currentRoll); 
       $("#turnScore-1").html(" " + player1.turnScore);
     } else {
-      $("#total-2").html(" " + player2.totalScore);
       alert("You rolled a 1! Your turn is over...")
-      $("#rollResult-1").html(" ");
-      $("#turnScore-1").html(" ");
-      $("#total-1").html(player1.totalScore);
-      $("#player1").hide();
-      $("#player2").show();
+      endTurnPlayerOne();
     }
   });
+
   $("form#player1Hold").submit(function(event) {
     event.preventDefault();
-    $("#rollResult-1").html(" ");
-    $("#turnScore-1").html(" ");
-    player1.endTurn;
-    $("#total-1").html(player1.totalScore);
-    $("#player1").hide();
-    $("#player2").show();
+    player1.endTurn();
+    endTurnPlayerOne();
   });
+
   $("form#player2Play").submit(function(event) {
     event.preventDefault();
     let roll = rollDie(player2);
+
     if (roll) {
       $("#rollResult-2").html(" " + player2.currentRoll); 
-      $("#turnscore-2").html(" " + player2.turnScore);
+      $("#turnScore-2").html(" " + player2.turnScore);
     } else {
       alert("You rolled a 1! Your turn is over...")
-      $("#rollResult-2").html(" "); 
-      $("#turnScore-2").html(" ");
-      $("#total-2").html(player2.totalScore);
-      $("#player2").hide();
-      $("#player1").show();
+      endTurnPlayerTwo();
     }
   });
+
   $("form#player2Hold").submit(function(event) {
     event.preventDefault();
-    $("#rollResult-2").html(" ");
-    $("#turnScore-2").html(" "); 
-    player2.endTurn;
-    $("#total-2").html(player2.totalScore);
-    $("#player2").hide();
-    $("#player1").show();
+    player2.endTurn();
+    endTurnPlayerTwo();
+  });
+
+  $("#resetGame").submit(function(event) {
+    event.preventDefault();
+    location.reload();
   });
 });
-
-/*let player1 = new Player($("input#playerOneName").val());
-let player2 = new Player($("input#playerTwoName").val());
-let player3 = "test";
-console.log(player1);*/
